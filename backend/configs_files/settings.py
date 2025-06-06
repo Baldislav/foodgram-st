a36deffd2f3a89beb347812a5a491a2a0413effb
Kirill Sysoev <kirill.sysoev2004@gmail.com>
2025-06-06 21:20:40 +0300

    review2 done

--- backend/configs_files/settings.py
+++ backend/configs_files/settings.py
@@ -58,8 +58,12 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
